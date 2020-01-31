5e658d6ea42cf4e621603624a0906a0e08c7d5b6
matbotW <mathewmcdade@gmail.com>
2020-01-30 23:44:27 -0800

    formatting logger message

--- app/api.go
+++ app/api.go
@@ -3,11 +3,13 @@ package app
 import (
 	"database/sql"
 	"fmt"
+	"log"
 	"net/http"
 )
 
 func ApiInfo(db *sql.DB) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("%s %s %s\n", r.RemoteAddr, r.Method, r.URL)
 		fmt.Fprintf(w, "Hello from the API!")
 	})
 }
